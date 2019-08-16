fee6e242178083a8a7d8b9513c86b45d54587857
panicbit <panicbit.dev@gmail.com>
2019-08-17 00:48:11 +0200

    Render ferris

--- arm11/src/main.rs
+++ arm11/src/main.rs
@@ -16,101 +16,6 @@ const SCREEN_HEIGHT: usize = 240;
 const SCREEN_TOP_FBSIZE: usize = (3 * SCREEN_TOP_WIDTH * SCREEN_HEIGHT);
 const SCREEN_BOTTOM_FBSIZE: usize = (3 * SCREEN_BOTTOM_WIDTH * SCREEN_HEIGHT);
 
-// static FB_TOP: [u32; SCREEN_TOP_WIDTH * SCREEN_HEIGHT] = [0x80; SCREEN_TOP_WIDTH * SCREEN_HEIGHT];
-
-// global_asm!(r#"
-// .section .text.start
-// .global _start
-// .align 4
-// @ .arm
-
-// _start:
-//     @ Disable caches / mpu
-//     mrc p15, 0, r4, c1, c0, 0  @ read control register
-//     bic r4, #(1<<12)           @ - instruction cache disable
-//     bic r4, #(1<<2)            @ - data cache disable
-//     bic r4, #(1<<0)            @ - mpu disable
-//     mcr p15, 0, r4, c1, c0, 0  @ write control register
-
-//     @ Clear bss
-//     @ldr r0, =__bss_start
-//     @ldr r1, =__end__
-//     @mov r2, #0
-
-//     @.bss_clr:
-//     @    cmp r0, r1
-//     @    strlt r2, [r0], #4
-//     @    blt .bss_clr
-
-//     @ Give read/write access to all the memory regions
-//     ldr r5, =0x33333333
-//     mcr p15, 0, r5, c5, c0, 2 @ write data access
-//     mcr p15, 0, r5, c5, c0, 3 @ write instruction access
-
-//     @ Sets MPU permissions and cache settings
-//     ldr r0, =0xFFFF001F	@ ffff0000 64k  | bootrom (unprotected / protected)
-//     ldr r1, =0x3000801B	@ 30000000 16k  | dtcm
-//     ldr r2, =0x01FF801D	@ 01ff8000 32k  | itcm
-//     ldr r3, =0x08000029	@ 08000000 2M   | arm9 mem (O3DS / N3DS) 
-//     ldr r4, =0x10000029	@ 10000000 2M   | io mem (ARM9 / first 2MB)
-//     ldr r5, =0x20000037	@ 20000000 256M | fcram (O3DS / N3DS)
-//     ldr r6, =0x1FF00027	@ 1FF00000 1M   | dsp / axi wram
-//     ldr r7, =0x1800002D	@ 18000000 8M   | vram (+ 2MB)
-//     mov r8, #0x2D
-//     mcr p15, 0, r0, c6, c0, 0
-//     mcr p15, 0, r1, c6, c1, 0
-//     mcr p15, 0, r2, c6, c2, 0
-//     mcr p15, 0, r3, c6, c3, 0
-//     mcr p15, 0, r4, c6, c4, 0
-//     mcr p15, 0, r5, c6, c5, 0
-//     mcr p15, 0, r6, c6, c6, 0
-//     mcr p15, 0, r7, c6, c7, 0
-//     mcr p15, 0, r8, c3, c0, 0	@ Write bufferable 0, 2, 5
-//     mcr p15, 0, r8, c2, c0, 0	@ Data cacheable 0, 2, 5
-//     mcr p15, 0, r8, c2, c0, 1	@ Inst cacheable 0, 2, 5
-
-//     @ Enable dctm
-//     ldr r1, =0x3000800A        @ set dtcm
-//     mcr p15, 0, r1, c9, c1, 0  @ set the dtcm Region Register
-
-//     @ Enable caches
-//     mrc p15, 0, r4, c1, c0, 0  @ read control register
-//     orr r4, r4, #(1<<18)       @ - itcm enable
-//     orr r4, r4, #(1<<16)       @ - dtcm enable
-//     orr r4, r4, #(1<<12)       @ - instruction cache enable
-//     orr r4, r4, #(1<<2)        @ - data cache enable
-//     orr r4, r4, #(1<<0)        @ - mpu enable
-//     mcr p15, 0, r4, c1, c0, 0  @ write control register
-
-//     @ Flush caches
-//     mov r5, #0
-//     mcr p15, 0, r5, c7, c5, 0  @ flush I-cache
-//     mcr p15, 0, r5, c7, c6, 0  @ flush D-cache
-//     mcr p15, 0, r5, c7, c10, 4 @ drain write buffer
-
-//     @ Fixes mounting of SDMC
-//     ldr r0, =0x10000020
-//     mov r1, #0x340
-//     str r1, [r0]
-
-//     mov sp, #0x27000000
-
-//     blx _rust_start
-//     b _start
-
-// .pool
-// "#);
-
-// #[no_mangle]
-// pub extern "C" fn _start() -> ! {
-//     unsafe {
-//         go(0x30000000, main);
-
-//         loop {}
-//     }
-// }
-
-
 global_asm!(r#"
 .section .text.start
 .global _start
@@ -125,6 +30,8 @@ _start:
 .pool
 "#);
 
+const FERRIS: &[u8] = include_bytes!("../../ferris.data");
+
 #[no_mangle]
 pub extern "C" fn _rust_start() -> ! {
     unsafe {
@@ -132,10 +39,6 @@ pub extern "C" fn _rust_start() -> ! {
         busy_sleep(1000);
 
         let fb_top = core::slice::from_raw_parts_mut::<[u8; 3]>(0x18000000 as *mut _, SCREEN_TOP_FBSIZE / 3);
-
-        for b in fb_top.iter_mut() {
-            *b = [0x00, 0xFF, 0x00];
-        }
 
         init_screens(fb_top);
     }
@@ -214,27 +117,10 @@ pub unsafe fn init_screens(top_fb: &mut [[u8; 3]]) {
 
     setup_framebuffers(top_fb.as_ptr() as _);
 
-    // busy_sleep(4000);
-
-    // Set color to yellow
-    // (*(0x10202204 as *mut Volatile<u32>)).write(0xFFFF0000);
-
-
-    // for b in top_fb.iter_mut() {
-    //     core::ptr::write_volatile(&mut b[0], 0x00);
-    //     core::ptr::write_volatile(&mut b[1], 0x00);
-    //     core::ptr::write_volatile(&mut b[2], 0x00);
-    // }
-
-    for (i, b) in top_fb.iter_mut().enumerate() {
-        let color = match i / ((SCREEN_TOP_WIDTH * SCREEN_HEIGHT) / 3) {
-            0 => [0x00, 0x00, 0xFF],
-            1 => [0x00, 0xFF, 0x00],
-            2 => [0xFF, 0x00, 0x00],
-            _ => [0x00, 0x00, 0xFF],
-        };
-
-        core::ptr::write_volatile(b, color);
+    for (pixel, ferris_pixel) in top_fb.iter_mut().zip(FERRIS.chunks(3)) {
+        core::ptr::write_volatile(&mut pixel[0], ferris_pixel[2]);
+        core::ptr::write_volatile(&mut pixel[1], ferris_pixel[1]);
+        core::ptr::write_volatile(&mut pixel[2], ferris_pixel[0]);
     }
 
     loop {}
